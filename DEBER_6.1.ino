c3f9a0522e8dc1a6e3f81cb12f0552f4642920ad
marisol93 <67400457+marisol93@users.noreply.github.com>
2020-07-03 11:39:04 -0500

    DEBER  6.1

---
+++ DEBER_6.1.ino
@@ -0,0 +1,28 @@
+String pass="1234";
+String letra="";
+int led1=8;//led verde
+int led2=7;//led rojo
+void setup() {
+  Serial.begin(9600);
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
+  Serial.println("Ingrese una contraseña");
+}
+
+void loop() {
+  if(Serial.available()>0){
+    letra=Serial.readString();
+    Serial.print(letra);
+    if(letra==pass){
+      digitalWrite(led1,HIGH);
+      digitalWrite(led2,LOW);
+      delay(200);
+    }
+    else{
+      digitalWrite(led2,HIGH);
+      digitalWrite(led1,LOW);
+    }
+  }
+  // put your main code here, to run repeatedly:
+
+}
